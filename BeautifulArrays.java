d9dfe844b706f85567b7020a4ae844891fde7271
bnkumar4u <bnkumar4u@gmail.com>
2017-06-06 20:31:09 +0530

    Beautiful arrays java prograam v-1.0 not working basic code

---
+++ BeautifulArrays.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class BeautifulArrays
+{
+	public static void main(String[] args)
+    {
+    	int t,n;
+    	Scanner scan =new Scanner(System.in);
+    	t=scan.nextInt();
+    	if(t>=1&&t<=1000000)
+    	{
+    		for (int i=0;i<t ;i++ ) 
+    		{
+    			n=scan.nextInt();
+    			if(n>=1&&n<=1000000)
+    			{
+    				int a[] = new int[n];
+    				for (int j=0;j<n ;j++ ) 
+    				{
+    					a[j]=scan.nextInt();	
+    				}
+    				for
+
+    			}
+    			
+    		}
+    	}
+		
+	}
+}
