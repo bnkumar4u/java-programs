451f6ea8bc4bfc261d1ba119f2c7b0574edeb033
bnkumar4u <bnkumar4u@gmail.com>
2017-06-06 19:14:00 +0530

    size of datatypes not working v-1.0

---
+++ SizeOf.java
@@ -0,0 +1,29 @@
+public class SizeOf
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("byte  "+sizeof(byte a));
+
+
+		System.out.println("int  "+sizeof(int));
+
+		System.out.println("short  "+sizeof(short));
+
+		System.out.println("long  "+sizeof(long));
+
+		System.out.println("long long :"+sizeof(long long));
+
+		System.out.println("long long long   :"+sizeof(long long long));
+
+		System.out.println("float  "+sizeof(float));
+
+		System.out.println("char  "+sizeof(char));
+
+		System.out.println("double  "+sizeof(double));
+
+		System.out.println("long double  "+sizeof(long double));
+
+		System.out.println("boolean  "+sizeof(boolean));
+		
+	}
+}
